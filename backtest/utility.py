e2a2d454384446d1c6f5d2f7453fc99ffd24d01f
linbozhong <linbozhong@gmail.com>
2019-12-25 14:15:55 +0800

    add get dominant time data

--- backtest/utility.py
+++ backtest/utility.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from typing import List
-from datetime import datetime
+from datetime import datetime， timedelta
 from collections import defaultdict
 
 from vnpy.trader.object import BarData, TradeData
@@ -57,3 +57,37 @@ def strip_digt(symbol: str) -> str:
         else:
             break
     return res
+
+
+def to_vt_symbol(rq_symbol: str) -> str:
+    """input symbol like RB1905.SHFE"""
+    symbol, exchange = rq_symbol.split('.')
+    if exchange in ['SHFE', 'DCE', 'INE']:
+        return '.'.join([symbol.lower(), exchange])
+    elif exchange in ['CZCE']:
+        for idx, char in enumerate(symbol):
+            if char.isdigit():
+                break
+        new_symbol = symbol[:idx] + symbol[idx+1:]
+        return '.'.join([new_symbol, exchange])
+    else:
+        return rq_symbol
+
+
+def get_dominant_in_periods(underlying: str, backtest_start: datetime, backtest_end: datetime) -> pd.DataFrame:
+    """
+    获取某个日期区间的主力合约数据
+    """
+    underlying = underlying.upper()
+    seg = pd.read_csv('dominant_data.csv', parse_dates=[1, 2])
+    
+    sel = seg[seg['underlying'] == underlying].copy()
+    sel.reset_index(inplace=True)
+    passed = sel[sel['start'] < backtest_start]
+    after = sel[sel['start'] > backtest_end]
+    passed_closest_idx = passed.index.values[-1]
+    after_first_idx = after.index.values[0] if not after.empty else len(sel)
+    
+    if passed.iloc[-1]['end'] - backtest_start < timedelta(days=37):
+        passed_closest_idx += 1
+    return sel[passed_closest_idx: after_first_idx].copy()
