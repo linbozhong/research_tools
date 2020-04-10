3da3034b7f7040eee5d0a678e0d37527c532d559
linbozhong <linbozhong@gmail.com>
2020-04-10 16:03:58 +0800

    option backtest add display log control

--- option_analyze/option_backtest.py
+++ option_analyze/option_backtest.py
@@ -30,6 +30,12 @@ class OptionBacktest():
 
         self.parameters = ['hedge_range', 'hedge_value']
 
+        self.is_log = False
+
+    def log(self, *arg, **kwargs):
+        if self.is_log:
+            print(*arg, **kwargs)
+
     def set_parameters(self, setting: dict):
         for name in self.parameters:
             if name in setting:
@@ -42,7 +48,7 @@ class OptionBacktest():
         self.trade_count += 1
         trade_id = f"backtest.{self.trade_count}"
         trade = (date, offset, price, trade_id)
-        # print(trade)
+        self.log(trade)
         self.trades.append(trade)
 
     def stats_result(self):
@@ -52,7 +58,7 @@ class OptionBacktest():
 
     def run_backtest(self):
         for _idx, bar in self.data.iterrows():
-            # print(bar.date, bar.open, bar.high, bar.low, bar.close)
+            self.log(bar.date, bar.open, bar.high, bar.low, bar.close)
             self.open_array.append(bar.open)
             self.close_array.append(bar.close)
 
@@ -61,7 +67,7 @@ class OptionBacktest():
 
             if not self.base_price:
                 open_ma = np.mean(self.open_array[-3:])
-                # print('open_ma:', open_ma)
+                self.log('open_ma:', open_ma)
                 if open_ma - bar.open > self.entry_range:
                     self.base_price = round(bar.open + self.entry_range, 2)
                 elif bar.open - open_ma > self.entry_range:
@@ -84,11 +90,11 @@ class OptionBacktest():
                         long_hedge = False
 
                     if long_hedge:
-                        # print('long-hedge')
+                        self.log('long-hedge:', 'high:', bar.high, 'target_high:', self.base_price + self.hedge_range)
                         self.base_price = self.base_price + self.hedge_value
                         self.hedge_long_count += 1
                     elif short_hedge:
-                        # print('short_hedge')
+                        self.log('short_hedge:', 'low:', bar.low, 'target_low:', self.base_price - self.hedge_range)
                         self.base_price = self.base_price - self.hedge_value
                         self.hedge_short_count += 1
                     else:
