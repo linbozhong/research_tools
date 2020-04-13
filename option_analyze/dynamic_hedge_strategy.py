b468e16e3e6d7687099c94d9e2952a69bdb02749
linbozhong <linbozhong@gmail.com>
2020-04-13 23:09:52 +0800

    dynamic hedge strategy finished

--- option_analyze/dynamic_hedge_strategy.py
+++ option_analyze/dynamic_hedge_strategy.py
@@ -19,15 +19,20 @@ class DynamicHedgeStrategy(CtaTemplate):
     base_price = 0
     entry_range = 0.05
 
-    hedge_range_percent = 2
-    hedge_multiple_percent = 50
-
-    hedge_range = 0.0
     gamma = 300
+    # 千分之
+    hedge_range_percent = 20
+    # 百分之
+    hedge_multiple_percent = 50
+    
+    hedge_range = 0.0
+    move_range = 0.0
+    hedge_size = 0
 
     last_trade_dt = None
 
-    parameters = ['hedge_range', 'hedge_multiple']
+
+    parameters = ['hedge_range_percent', 'hedge_multiple_percent']
     variables = []
 
     def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
@@ -39,8 +44,7 @@ class DynamicHedgeStrategy(CtaTemplate):
         self.bg5 = BarGenerator(self.on_bar, 5, self.on_5min_bar)
         self.am5 = ArrayManager(size=20)
 
-        # self.hedge_size = round(self.hedge_range * self.gamma)
-        # print("params:", self.hedge_range, self.hedge_multiple, self.hedge_size)
+        print("params:", self.hedge_range_percent, self.hedge_multiple_percent)
 
     def on_init(self):
         self.write_log("策略初始化")
@@ -68,32 +72,36 @@ class DynamicHedgeStrategy(CtaTemplate):
         if not self.am5.inited:
             return
 
+
+        # 初始入场，确定首次对冲范围和对冲基准手数
         if not self.base_price:
             self.base_price = round(bar.open_price, 2)
+            self.hedge_range = self.base_price * (self.hedge_range_percent / 1000)
+            self.hedge_size = round(self.hedge_range * self.gamma)
         else:
             up = self.base_price + self.hedge_range
             down = self.base_price - self.hedge_range
-            self.log('base:', self.base_price, 'up:', up, 'down:', down, 'pos:', self.pos)
+            self.log('base:', self.base_price, 'up:', up, 'down:', down, 'pos:', self.pos, 'range:', self.hedge_range)
+
+            action_size = round(self.hedge_size * (self.hedge_multiple_percent / 100))
 
             if self.pos == 0:
-                self.buy(up, self.hedge_size * self.hedge_multiple, True)
-                self.short(down, self.hedge_size * self.hedge_multiple, True)
+                self.buy(up, action_size, True)
+                self.short(down, action_size, True)
             elif self.pos > 0:
                 # 条件触发用本地单
-                self.sell(down, self.hedge_size * self.hedge_multiple, True)
-                # self.short(down, self.hedge_size * self.hedge_multiple, True)
-                self.buy(up, self.hedge_size * self.hedge_multiple, True)
+                self.sell(down, abs(self.pos), True)
+                self.buy(up, action_size, True)
 
                 # 多单止盈用限价单
-                self.sell(up, self.hedge_size * self.hedge_multiple, False)
+                self.sell(up, abs(self.pos), False)
             elif self.pos < 0:
                 # 条件触发用本地单
-                self.cover(up, self.hedge_size * self.hedge_multiple, True)
-                # self.buy(up, self.hedge_size * self.hedge_multiple, True)
-                self.short(down, self.hedge_size * self.hedge_multiple, True)
+                self.cover(up, abs(self.pos), True)
+                self.short(down, action_size, True)
 
                 # 空单止盈用限价单
-                self.cover(down, self.hedge_size * self.hedge_multiple, False)
+                self.cover(down, abs(self.pos), False)
 
     def on_order(self, order: OrderData):
         pass
@@ -101,21 +109,24 @@ class DynamicHedgeStrategy(CtaTemplate):
     def on_trade(self, trade: TradeData):
         self.log("Trade:", trade.datetime, trade.direction, trade.offset, trade.price, trade.volume)
         if trade.datetime != self.last_trade_dt:
+            # 和上次成交不是同一个时间的成交单，是下一个对冲点的第一笔成交（或只有一笔成交）
+            self.hedge_range = self.base_price * (self.hedge_range_percent / 1000)
+            self.move_range = round(self.hedge_range * (self.hedge_multiple_percent / 100), 2)
+            self.hedge_size = round(self.move_range * self.gamma)
+
             if trade.direction == Direction.LONG:
-                self.base_price += self.hedge_range * self.hedge_multiple
+                self.base_price += self.move_range
             elif trade.direction == Direction.SHORT:
-                self.base_price -= self.hedge_range * self.hedge_multiple
+                self.base_price -= self.move_range
         else:
             # 如果平仓后跟着开仓，说明前一次基线调错了，补回来后还要相应移动1档
             # self.log('trades at the same time')
             if trade.direction == Direction.LONG and trade.offset == Offset.OPEN:
-                self.base_price += self.hedge_range * self.hedge_multiple * 2
+                self.base_price += self.move_range * 2
             elif trade.direction == Direction.SHORT and trade.offset == Offset.OPEN:
-                self.base_price -= self.hedge_range * self.hedge_multiple * 2
-
+                self.base_price -= self.move_range * 2
         
         self.last_trade_dt = trade.datetime
-
 
     def on_stop_order(self, stop_order: StopOrder):
         pass
