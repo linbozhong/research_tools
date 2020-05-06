b49918eb9d6bd67b53af1c02a95f6934726ba5a8
linbozhong <linbozhong@gmail.com>
2020-05-06 22:23:09 +0800

    option analyze add 510300

--- option_analyze/qvix.py
+++ option_analyze/qvix.py
@@ -9,25 +9,43 @@ from datetime import datetime
 
 from utility import dt_to_str
 
-QVIX_URL = 'http://1.optbbs.com/d/csv/d/vixk.csv'
+QVIX_URL_510050 = 'https://1.optbbs.com/d/csv/d/vixk.csv'
+QVIX_URL_510300 = 'https://1.optbbs.com/d/csv/d/vixk300.csv'
+
+QVIX_URL_MAP = {
+    '510050': QVIX_URL_510050,
+    '510300': QVIX_URL_510300
+}
+
+QVIX_FILENAME_MAP = {
+    '510050': 'vix_bar.csv',
+    '510300': 'vix_bar_510300.csv'
+}
 
 
-def get_qvix_file():
-    return Path.cwd().joinpath('vix_bar.csv')
+def get_qvix_file(underlying: str = '510050'):
+    filename = QVIX_FILENAME_MAP[underlying]
+    return Path.cwd().joinpath(filename)
 
-def get_qvix():
-    fp = get_qvix_file()
+
+def get_qvix(underlying: str = '510050'):
+    fp = get_qvix_file(underlying)
     return pd.read_csv(fp, index_col=0, parse_dates=True)
 
 
-def update_qvix():
+def update_qvix(underlying: str = '510050'):
+    qvix_url = QVIX_URL_MAP.get(underlying)
+    if qvix_url is None:
+        print('unsupport underlying.')
+        return
+
     try:
-        resp = requests.get(QVIX_URL)
+        resp = requests.get(qvix_url)
         csv_strings_iterator = (line.decode('utf-8')
                                 for line in resp.iter_lines())
         csv_data = list(csv.DictReader(csv_strings_iterator))
     except:
-        print('error occur when get qvix.')
+        print(f'error occur when get {underlying} qvix.')
     else:
         name_dict = {
             '1': 'datetime',
@@ -47,13 +65,13 @@ def update_qvix():
             lambda t_stamp_str: dt_to_str(datetime.fromtimestamp(float(t_stamp_str) / 1000)))
 
         df.set_index('datetime', inplace=True)
-        df.to_csv(get_qvix_file())
-        print('update completely!')
+        df.to_csv(get_qvix_file(underlying))
+        print(f'{underlying} vix daily bar update completely!')
         return df
 
 
-def get_hist_vol():
-    df = ts.get_k_data('510050', start='2005-02-23')
+def get_hist_vol(underlying: str = '510050'):
+    df = ts.get_k_data(underlying, start='2005-02-23')
     df['return_sim'] = df['close'].pct_change()
     df['return_log'] = np.log(df['close'] / df['close'].shift(1))
 
